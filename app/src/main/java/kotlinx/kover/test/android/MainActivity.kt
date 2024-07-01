76d5e66bdbd2904bd62d607902d5e637671ccd66
Nathan Lecoanet <nl@datatheorem.io>
2024-07-01 10:25:19 +0200

    Add MP 24.0.1 with excluded task

--- app/src/main/java/kotlinx/kover/test/android/MainActivity.kt
+++ app/src/main/java/kotlinx/kover/test/android/MainActivity.kt
@@ -2,12 +2,14 @@ package kotlinx.kover.test.android
 
 import android.os.Bundle
 import android.app.Activity
+import com.datatheorem.mobileprotect.MobileProtect
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        MobileProtect.init(this.application, R.xml.mobileprotect)
     }
 
 }
